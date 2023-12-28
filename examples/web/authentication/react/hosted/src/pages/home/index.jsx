c577950939c4c41d7cb45f370b772b6fd002b5e7
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2023-12-28 09:34:29 +0530

    chore: added docs in hosted auhentication example

--- examples/web/authentication/react/hosted/src/pages/home/index.jsx
+++ examples/web/authentication/react/hosted/src/pages/home/index.jsx
@@ -22,7 +22,9 @@ export default function HomePage() {
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                 Get the source code here.{" "}
                 <a
-                  href="#"
+                  href="https://github.com/muthaiyanmani/catalyst/tree/main/examples/web/authentication/react/hosted"
+                  target="_blank"
+                  rel="noreferrer"
                   className="font-semibold cursor-pointer text-indigo-600"
                 >
                   <span className="absolute inset-0" aria-hidden="true" />
